d9f50181359d798367f10cc32cf9f5f93fecf4c5
Gabriel Pinheiro Cardoso da Silva <gabriel.pinheirocds@gmail.com>
2026-04-05 11:54:29 -0300

    feat: criação da classe Agendamento e mapeamento das chaves estrangeiras

---
+++ BioCiclo.App/Models/Agendamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BioCiclo.App.Models
+{
+    public class Agendamento
+    {
+        public int ID_Agendamento { get; set; }
+        public DateTime Data_Agendamento { get; set; }
+        public string Status_Coleta { get; set; } = "Pendente";
+
+        // Chaves Estrangeiras (FK) do seu banco
+        public int FK_Usuario { get; set; }
+        public int FK_Material { get; set; }
+    }
+}
