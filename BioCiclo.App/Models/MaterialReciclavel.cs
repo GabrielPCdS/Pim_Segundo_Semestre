204cdf6d54cd2bd4b2699bb630a4ba179784bd11
Gabriel Pinheiro Cardoso da Silva <gabriel.pinheirocds@gmail.com>
2026-04-05 11:56:41 -0300

    feat: finalização do domínio com a classe MaterialReciclavel e ajuste de visibilidade

---
+++ BioCiclo.App/Models/MaterialReciclavel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BioCiclo.App.Models
+{
+    public class MaterialReciclavel
+    {
+        // PK - Identificador único do material
+        public int ID_Material { get; set; }
+
+        // Descrição (Ex: Papelão, Vidro, Plástico)
+        public string Descricao { get; set; }
+
+        // Construtor padrão
+        public MaterialReciclavel() { }
+
+        // Construtor auxiliar para facilitar a criação
+        public MaterialReciclavel(int id, string descricao)
+        {
+            ID_Material = id;
+            Descricao = descricao;
+        }
+    }
+}
