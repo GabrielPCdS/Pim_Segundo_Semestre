63cb1e3695bef26c3e50a4d970de5a96da2a5e23
Gabriel Pinheiro Cardoso da Silva <gabriel.pinheirocds@gmail.com>
2026-04-05 11:48:16 -0300

    feat: Implementação da herança com as classes Cliente e Cooperativa

---
+++ BioCiclo.App/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace BioCiclo.App.Models
+{
+    // Cliente herda tudo de Usuario
+    public class Cliente : Usuario
+    {
+        public void SolicitarColeta()
+        {
+            Console.WriteLine("Iniciando fluxo de agendamento de resíduos...");
+        }
+    }
+}
---
+++ BioCiclo.App/Models/Coperativa.cs
@@ -0,0 +1,11 @@
+﻿namespace BioCiclo.App.Models
+{
+    // Cooperativa também herda de Usuario
+    public class Cooperativa : Usuario
+    {
+        public void ConfirmarColeta()
+        {
+            Console.WriteLine("Confirmando recebimento de material reciclável...");
+        }
+    }
+}
